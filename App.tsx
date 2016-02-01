a6d23d69eb8c7e67b6aa053941fb5dcf3e71adad
Danny Libin <omegasol11@gmail.com>
2016-01-31 22:03:13 -0600

    creating design skeleton

--- App.tsx
+++ App.tsx
@@ -1,8 +1,37 @@
 import * as React from 'react';
 
 class App extends React.Component<any, any> {
+	
+	constructor() {
+		super();
+		this.state = {
+			topRecentCampers: [],
+			topAlltimeCampers: []
+		}
+	}
+	
 	render() {
-		return <div>Hello Alice</div>
+		return (
+			<div id="page-wrapper">
+				<div id="header">
+					<a href="http://www.freecodecamp.com/">
+						<img src="https://s3.amazonaws.com/freecodecamp/freecodecamp_logo.svg" id="fcc-logo"/>
+					</a>
+				</div>
+				<Leaderboard />
+			</div>	
+		);
+	}
+}
+
+class Leaderboard extends React.Component<any, any> {
+	render() {
+		return (
+			<div id="table-wrapper">
+				<div className="row" id="row-title">Leaderboard</div>
+				<div className="row" id="row-category"></div>
+			</div>
+		);
 	}
 }
 
